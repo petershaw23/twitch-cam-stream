02771a7aec980f29dc5810f2c9d0a7e78b5df654
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2019-10-30 21:00:47 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 cd "${0%/*}"
 echo $PWD
-ffmpeg -re -stream_loop -1 -i bg2.mp4 -c copy -f flv  "rtmp://live.twitch.tv/app/live_468692882_Om70M4YuOXQAByrM5EALU35Tiiqb9z"
+ffmpeg -re -stream_loop -1 -i bg2.mp4 -c copy -f flv  "rtmp://live.twitch.tv/app/live_XXXXXXXXXXX"
