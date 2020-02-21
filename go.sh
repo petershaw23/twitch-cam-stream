ba78e54bf00fce7f18dd1630267b31917613a595
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-02-21 19:53:53 +0100

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,5 +4,5 @@ echo $PWD
 sleep 15
 
 # 720p 30 fps (15% CPU)
-./ffmpeg -f video4linux2 -s 1280x720 -framerate 25 -i /dev/video0 -c:v h264_omx -threads 0 -an -f flv -b:v 1500k -bufsize 1500k "rtmp://live.twitch.tv/app/live_XXXXXXXXX"
+ffmpeg -f alsa -acodec pcm_s24le -ac 2 -ar 44100 -i hw:1,0 -f video4linux2 -i /dev/video0 -c:v h264_omx -threads 0 -f flv "rtmp://live.twitch.tv/app/live_468577072_gtuKjNqUztm8iocFqYFOientuG0gFk"
 
